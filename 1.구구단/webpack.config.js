31f7567419f8abb9366105a94a1d2c6452d89a89
jeonghyeonuk <junghunwook456@naver.com>
2023-01-05 16:23:09 +0900

    zerocho react 2-7까지

--- 1.구구단/webpack.config.js
+++ 1.구구단/webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
-
 module.exports = {
   mode: 'development',
-  devtool: 'inline-source-map', // hidden-source-map
+  devtool: 'eval', // 서비스일때는 hidden-source-map
   resolve: {
-    extensions: ['.jsx', '.js'],
-  },
+    extensions: ['.js', '.jsx'],
+  }, // entry에 파일명 넣을때 확장자 가능하게 만들어줌
 
   entry: {
     app: './client',
-  },
+  }, // 입력
+
   module: {
     rules: [{
       test: /\.jsx?$/,
@@ -19,18 +19,27 @@ module.exports = {
         presets: [
           ['@babel/preset-env', {
             targets: {
-              browsers: ['> 1% in KR'], // browserslist
+              browsers: ['> 5% in KR', 'last 2 chrome versions']
             },
-            debug: true,
+            /* preset-env 가 최신코드가 IE 같은곳에서 안돌아갈때 그걸 자동으로 돌아가게 바꿔주는건데, 
+            여기서 targets 속성을 사용해서 내가 지원하고자 하는 브라우저를 따로 선택해줄수있다.
+            이걸 정해주지않으면 모든 브라우저를 지원하게되고 그렇게되면 속도가 느려진다*/
           }],
-          '@babel/preset-react',
+          '@babel/preset-react'
         ],
         plugins: [],
       },
     }],
-  },
+  }, // 설정
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    }),
+  ], //추가작업
+
   output: {
     filename: 'app.js',
     path: path.join(__dirname, 'dist'),
-  },
-};
+  }, //출력
+}
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,49 +1,31 @@
 const path = require('path');
-const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-  name: 'wordrelay',
-  mode: 'development', // 실서비스: production
-  devtool: 'eval', // 실서비스: hidden-source-map
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
+	name : 'word-realy-setting', //이름
+	mode : 'development', // 실서비스 : production
+	devtool : 'eval', // 빠르게해줌
+	resolve : {
+		extensions : ['.js', '.jsx']
+	}, // entry에 파일명 넣을때 확장자 가능하게 만들어줌
 
-  entry: {
-    app: ['./client'],
-  }, // 입력
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          ['@babel/preset-env', {
-            targets: {
-              browsers: ['> 1% in KR'], // browserslist
-            },
-            debug: true,
-          }],
-          '@babel/preset-react',
-        ],
-        plugins: [
-          '@babel/plugin-proposal-class-properties',
-          'react-refresh/babel',
-        ],
-      },
-    }],
-  },
-  plugins: [
-    new RefreshWebpackPlugin()
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.js',
-    publicPath: '/dist/',
-  },
-  devServer: {
-    devMiddleware: { publicPath: '/dist' },
-    static: { directory: path.resolve(__dirname) },
-    hot: true
-  },
+	entry : {
+		app:['./client'],
+	}, // 입력
+
+	module : {
+		rules:[{
+			test:/\.jsx?/,
+			loader:'babel-loader',
+			options:{
+				presets:['@babel/preset-env', '@babel/preset-react'],
+				plugins:['@babel/plugin-proposal-class-properties'],
+			},
+		}]
+	},
+
+	output : {
+		path: path.join(__dirname, 'dist'),
+		filename: 'app.js'
+	},// 출력
+
 };
