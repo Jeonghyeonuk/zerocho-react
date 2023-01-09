d7a8a3345df7f5f19e1bc7d210eba3958e724554
jeonghyeonuk <junghunwook456@naver.com>
2023-01-09 14:19:19 +0900

    2-10까지

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -2,39 +2,40 @@ const React = require('react');
 const { useState, useRef } = React;
 
 const WordRelay = () => {
-  const [word, setWord] = useState('제로초');
-  const [value, setValue] = useState('');
-  const [result, setResult] = useState('');
-  const inputEl = useRef(null);
+	const [word, setWord] = useState('김밥');
+	const [value, setValue] = useState('');
+	const [result, setResult] = useState('');
+	const inputRef = useRef(null);
 
-  const onSubmitForm = (e) => {
-    e.preventDefault();
-    if (word[word.length - 1] === value[0]) {
-      setResult('딩동댕');
-      setWord(value);
-      setValue('');
-      inputEl.current.focus();
-    } else {
-      setResult('땡');
-      setValue('');
-      inputEl.current.focus();
-    }
-  };
+	const onSubmitForm = (e) => {
 
-  return (
-    <>
-      <div>{word}</div>
-      <form onSubmit={onSubmitForm}>
-        <input
-          ref={inputEl}
-          value={value}
-          onChange={(e) => setValue(e.currentTarget.value)}
-        />
-        <button>입력!</button>
-      </form>
-      <div>{result}</div>
-    </>
-  );
-};
+		e.preventDefault();
+		if (word[word.length - 1] === value[0]) {
+			setResult('딩동댕');
+			setWord(value);
+			setValue('');
+		} else {
+			setResult('땡');
+			setValue('');
+		}
+		inputRef.current.focus();
+	}
+
+	const onChangeInput = (e) => {
+		setValue(e.target.value);
+	}
+
+
+	return (
+		<>
+			<div>{word}</div>
+			<form onSubmit={onSubmitForm}>
+				<input ref={inputRef} value={value} onChange={onChangeInput} />
+				<button>입력!</button>
+			</form>
+			<div>{result}</div>
+		</>
+	)
+}
 
 module.exports = WordRelay;
