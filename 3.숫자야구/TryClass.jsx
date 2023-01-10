6a96f37074de97c6b5e918ae7462c71e51718228
jeonghyeonuk <junghunwook456@naver.com>
2023-01-10 16:27:37 +0900

    웹게임 3-7까지

--- 3.숫자야구/TryClass.jsx
+++ 3.숫자야구/TryClass.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 class Try extends Component {
   render() {
-    const { tryInfo } = this.props;
+    const {tryInfo} = this.props;
     return (
+      <>
       <li>
         <div>{tryInfo.try}</div>
         <div>{tryInfo.result}</div>
       </li>
+      </>
     );
   }
 }
